6610f8ad14608e27f902396949dc81f1d6cfffb3
eleonora-roncaglia_Boskalis <eleonora.roncaglia@boskalis.com>
2026-06-01 15:21:50 +0200

    fixed data import

--- src/data_processor.py
+++ src/data_processor.py
@@ -4,40 +4,27 @@ import os
 from config import *
   
 class DataProcessor:
-    def __init__(self, name, max_history=MAX_HISTORY):
+    def __init__(self, name):
         self.name = name
-        self.max_history = max_history
         self.df = self._load_data()
-        self.user_histories = self._build_user_histories()
-        self.metadata = self._load_metadata()
+        # self.metadata = self._load_metadata()
     
     def _load_data(self):
         try:
             df = pd.read_csv(f"data/{self.name}")
+            if self.name in ["train.csv", "test.csv"]:
+                df = df.sort_values(["user_id", "timestamp"])
             return df
         except FileNotFoundError:
             print(f"File not found: {self.name}")
             return pd.DataFrame()
-
-    def _load_metadata(self):
-        for fname in os.listdir("data/"):
-            if os.path.isfile("data/" + os.sep + fname):
-                # Full path
-                f = open("data/" + os.sep + fname, 'r')
-
-                if "meta" in f.read():
-                    meta = pd.read_csv("data/" + os.sep + fname)
-                else:
-                    raise FileNotFoundError("Metadata file not found in data directory.")
-                f.close()
-        return meta
     
-    def join_data(self):
+    def add_embeddings(self, embeddings):
         # Join the data and metadata on the parent_asin column
-        joined_df = pd.merge(self.df, self.metadata, on="item_id", how="left")
+        joined_df = pd.merge(self.df, embeddings, on="item_id", how="left")
         return joined_df
 
-    def process_data(self):
+    def add_sequence(self):
       self.df["sequence"] = (
           self.df["main_category"].astype(str) +
           self.df["title"].astype(str) +
@@ -47,15 +34,5 @@ class DataProcessor:
           self.df["description"].astype(str) +
           self.df["categories"].astype(str)
       )
-      self.df = self.df.sort_values(["user_id", "timestamp"])
       return self.df
 
-    def _build_user_histories(self):
-        user_histories = {}
-        for user_id, group in self.df.groupby("user_id"):
-            user_histories[user_id] = group["sequence"].tolist()
-        return user_histories
-    
-    def get_user_history(self, user_id):
-        history = self.user_histories.get(user_id, [])
-        return history[-self.max_history:]
